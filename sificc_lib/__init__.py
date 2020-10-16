bf08894a8da462044b01247a364396119a90a066
Awal Awal <awal.nova@gmail.com>
2020-10-16 10:15:53 +0200

    Initial commit with the modules

---
+++ sificc_lib/__init__.py
@@ -0,0 +1,10 @@
+# pip install tensorflow==2.2 numpy jupyterlab uproot matplotlib tqdm pandas xxhash lz4
+
+from sificc_lib.root_files import root_files
+
+from sificc_lib.utils import utils
+from sificc_lib.SiFiCC_Module import SiFiCC_Module
+from sificc_lib.Event import Event
+from sificc_lib.Simulation import Simulation
+from sificc_lib.DataModel import DataModel
+from sificc_lib.AI import AI
---
+++ sificc_lib/root_files.py
@@ -0,0 +1,14 @@
+class root_files:
+    HPC_LOW = '/hpcwork/ae664232/InputforNN/PMMA180MeV0mmBPType3_LowStatistics.root'
+    HPC_HIGHER = '/hpcwork/ae664232/InputforNN/PMMA180MeV0mmBPType3_HigherStatistics.root'
+    HPC_NEW = '/hpcwork/ae664232/InputforNN/PMMA180MeV0mmBPType3_NewStatistics.root'
+    HPC_HIGHEST = '/hpcwork/ae664232/InputforNN/PMMA180MeV0mmBPType3_HighestStatistics.root'
+    HPC_ENOUGH = '/hpcwork/ae664232/InputforNN/PMMA180MeV0mmBPType3_EnoughStatistics.root'
+    LOCAL_LOW = '/home/awal/Desktop/others-thesis/PMMA180MeV0mmBPType3_LowStatistics.root'
+    LOCAL_HIGHER = '/home/awal/Desktop/others-thesis/PMMA180MeV0mmBPType3_HigherStatistics.root'
+    PZ_LOW = '/net/data_g4rt/projects/SiFiCC/InputforNN/PMMA180MeV0mmBPType3_LowStatistics.root'
+    PZ_HIGHER = '/net/data_g4rt/projects/SiFiCC/InputforNN/PMMA180MeV0mmBPType3_HigherStatistics.root'
+    PZ_NEW = '/net/data_g4rt/projects/SiFiCC/InputforNN/PMMA180MeV0mmBPType3_NewStatistics.root'
+    PZ_HIGHEST = '/net/data_g4rt/projects/SiFiCC/InputforNN/PMMA180MeV0mmBPType3_HighestStatistics.root'
+    PZ_ENOUGH = '/net/data_g4rt/projects/SiFiCC/InputforNN/PMMA180MeV0mmBPType3_EnoughStatistics.root'
+    
